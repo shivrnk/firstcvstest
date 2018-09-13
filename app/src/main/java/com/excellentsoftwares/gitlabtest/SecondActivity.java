57661b2226b745c4be1da293ef68051e80fdf6ad
Shiv <myname.shiva2011@gmail.com>
2018-09-13 17:28:32 +0530

    Add method

--- app/src/main/java/com/excellentsoftwares/gitlabtest/SecondActivity.java
+++ app/src/main/java/com/excellentsoftwares/gitlabtest/SecondActivity.java
@@ -9,5 +9,14 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        Add();
+
+    }
+    void Add()
+    {
+        int a,b,c;
+        a=10;
+        b=5;
+        c=a+b;
     }
 }
